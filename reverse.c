7164eebe43e1b9719e49f99e409cb241b426fae6
Sham-98 <130893083+Sham-98@users.noreply.github.com>
2025-11-03 09:17:24 +0200

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,22 +1,72 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
-int main(int argc, char *argv[]) {
+
+  int main(void){ 
+    FILE *file = fopen("input.txt", "r");
+    if (file == NULL) {
+        fprintf(stderr, "error: cannot open file 'input.txt'\n");
+        exit (1);
+    }
+
+    char *line = NULL; // for getline
+    size_t len = 0;
+    ssize_t read;
+
+    //donamic array for storing lines
+    char **lines = NULL; 
+    size_t capacity = 0;
+    size_t count = 0;
 
 
-if (argc > 3) {
-    fprintf(stderr, "usage: reverse <input> <output>\n");
-    exit(1);
-}
 
-FILE *inf = fopen(argv[1], "r");
-if (!inf) {
-    fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
-    exit(1);
-}
-    printf("File opened successfully: %s\n", argv[1]);
+    while ((read = getline(&line, &len, file)) != -1) {
+      if (count == capacity){
+        capacity = (capacity == 0) ? 10 : capacity * 2;
+        lines = realloc(lines, capacity * sizeof(char *));
+        if (lines == NULL) {
+          fprintf(stderr, "malloc failed\n");
+          free(line);
+          fclose(file);
+          exit(1);
+}}
+ // Allocate space for line and copy 
+        lines[count] = malloc(strlen(line) + 1);
+        if (lines[count] == NULL) {
+            fprintf(stderr, "malloc failed for line\n");
+            free(line);
+            fclose(file);
+            exit(1);
+        }
+        strcpy(lines[count], line);
+        count++;
+    }
+    fclose(file);
+    free(line);
 
-    fclose(inf);
+// Open output file for w
+    FILE *outfile = fopen("output.txt", "w");
+    if (outfile == NULL) {
+        fprintf(stderr, "error: cannot open file 'output.txt'\n");
+        for (size_t i = 0; i < count; i++)
+          free(lines[i]);
+        free(lines);
+        exit(1);
+    }
+
+    // print in revers order 
+    for (ssize_t i = count - 1; i >= 0; i--) {
+        fprintf(outfile, "%s", lines[i]);
+        free(lines[i]);
+    }
+
+
+    free(lines);
+    fclose(outfile);
     return 0;
 }
+  
 
